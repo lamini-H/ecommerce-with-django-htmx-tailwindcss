6fa600eeb81871cf77ae3682696a5a49d1a952a6
lamini-H <113555226+lamini-H@users.noreply.github.com>
2023-07-04 16:23:25 +0000

    Add files via upload

---
+++ core/urls.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+
+from django.urls import path, include
+
+
+
+
+urlpatterns = [
+    
+    path('', include('main.urls')),
+    path('', include('cart.urls')),
+    path('order/', include('order.urls')),
+    path('admin/', admin.site.urls),
+
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
